f53f7f0844d0f74949786f2fff297e87195de933
lyj00912 <3122742760@qq.com>
2022-01-15 23:16:34 +0800

    renamed T03, also created T05WhileAndIfStatement.m

---
+++ T05WhileAndIfStatement.m
@@ -0,0 +1,52 @@
+clear
+clc
+
+% % Define Bool Variables
+% x = true
+% y = false
+
+% % Infinity While Loop
+% while true
+%     % Do sth
+% end
+
+% % While Loop Example
+% x = 3
+% while x > 0
+%     x = x - 1
+% end
+
+% If Statement
+x = 9;
+if x > 10
+    'x > 10!'
+elseif x == 10
+    'x == 10'
+else
+    'x < 10!'
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
