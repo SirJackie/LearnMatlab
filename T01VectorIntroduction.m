da246b496f37956811a0418b2d63cabb4475e914
lyj00912 <3122742760@qq.com>
2022-01-15 14:00:36 +0800

    created T01VectorIntroduction.m

---
+++ T01VectorIntroduction.m
@@ -0,0 +1,31 @@
+% % About Filename
+% % You Can't use "-" in filename, it will be recognized as minus symbol
+% % You Can't use "_" in filename neither
+% % You Can't use letter F in filename due to "F"unction
+% % e.g. F01VectorIntroduction
+% % ===> 01*VectorIntroduction01*VectorIntroduction
+
+% % About Comment
+% % Matlab use neither "//" nor "#" for comment
+% % It uses "%" symbol
+% % Commenting Shortcuts:
+% % Ctrl + R to comment
+% % Ctrl + T to un-comment
+% % You can modify this shortcut by clicking 主页-预设-键盘-快捷方式
+
+% % Vector Definition
+% % Unlike Sympy
+% % There's no "," between each entry
+% v = [3 1]
+
+% % Console Output
+% % Without semicolon: result will be printed
+% % With semicolon: result won't be printed
+% v = [3 1];
+
+% % Cleaning Commands
+% clear  % Clear the workspace
+% clc    % Clear the console
+% clf    % Clear the figure
+
+x = linspace(0, 10, 10)
